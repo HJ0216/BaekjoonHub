a7d1df8588e4873f877fe6b379b0f1e109ee7db4
Hyunji <6120hj@gmail.com>
2024-06-30 06:42:35 +0900

    [Bronze II] Title: 거스름돈, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -8,32 +8,14 @@ public class Main {
 
         int changes = 1000 -Integer.parseInt(br.readLine());
 
+        int[] coins = {500, 100, 50, 10, 5, 1};
+
         int countCoins = 0;
 
-        while (changes >0){
-            if(changes >= 500){
-                countCoins += changes / 500;
-                changes -= 500*(changes / 500);
-            }
-            if(changes >= 100){
-                countCoins += changes / 100;
-                changes -= 100*(changes / 100);
-            }
-            if(changes >= 50){
-                countCoins += changes / 50;
-                changes -= 50*(changes / 50);
-            }
-            if(changes >= 10){
-                countCoins += changes / 10;
-                changes -= 10*(changes / 10);
-            }
-            if(changes >= 5){
-                countCoins += changes / 5;
-                changes -= 5*(changes / 5);
-            }
-            if(changes >= 1){
-                countCoins += changes / 1;
-                changes -= 1*(changes / 1);
+        for (int i=0; changes>0; i++){
+            if(changes >= coins[i]){
+                countCoins += (changes / coins[i]);
+                changes -= coins[i] * (changes / coins[i]);
             }
         }
 
