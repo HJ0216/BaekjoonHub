2b792fb7e71ed305ef87bc2201da1e9b74b7c3e6
Hyunji <6120hj@gmail.com>
2024-07-07 03:06:43 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,11 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static boolean[] check;
-    static int[][] arr;
+    static boolean[] visited;
+    static ArrayList<Integer>[] adjacencyList;
     static int count = 0;
     static int node, line;
-    static Queue<Integer> queue = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,31 +14,35 @@ public class Main {
         node = Integer.parseInt(br.readLine());
         line = Integer.parseInt(br.readLine());
 
-        arr = new int[node+1][node+1];
-        check = new boolean[node+1];
+        adjacencyList = new ArrayList[node+1];
+        visited = new boolean[node+1];
 
-        for (int i=0; i<line; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            int nodeA = Integer.parseInt(st.nextToken());
-            int nodeB = Integer.parseInt(st.nextToken());
-
-            arr[nodeA][nodeB] = arr[nodeB][nodeA] = 1;
+        for (int i=1; i<=node; i++){
+            adjacencyList[i] = new ArrayList<Integer>();
         }
-        
+
+        for(int i=0; i<line; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+
+            adjacencyList[s].add(e);
+            adjacencyList[e].add(s);
+        }
+
         dfs(1);
 
-        bw.write(count-1+"");
+        bw.write(count+"");
         bw.flush();
         bw.close();
     }
 
     private static void dfs(int start) {
-        check[start] = true;
-        count++;
+        visited[start] = true;
 
-        for (int i=0; i<= node; i++){
-            if(arr[start][i] == 1 && !check[i]){
+        for (int i : adjacencyList[start]){
+            if(!visited[i]){
+                count++;
                 dfs(i);
             }
         }
