7f6890e6cdfdc65602d2040fc640acdb67e2a9a1
Hyunji <6120hj@gmail.com>
2024-07-07 07:46:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 256 ms, Memory: 20912 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,13 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static boolean[] visitedDfs;
-    static boolean[] visitedBfs;
-    static int[][] adjacencyMatrix;
+    static boolean[] visited;
+    static ArrayList<Integer>[] adjacencyList;
     static int node, line;
 
-    static StringBuilder sbDfs = new StringBuilder();
-    static StringBuilder sbBfs = new StringBuilder();
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,58 +17,67 @@ public class Main {
         line = Integer.parseInt(st.nextToken());
         int v = Integer.parseInt(st.nextToken());
 
-        adjacencyMatrix = new int[node+1][node+1];
-        visitedDfs = new boolean[node+1];
-        visitedBfs = new boolean[node+1];
+        adjacencyList = new ArrayList[node+1];
+        visited = new boolean[node+1];
 
-        for(int i=0; i<line; i++){
-            st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-
-            adjacencyMatrix[s][e] = adjacencyMatrix[e][s] = 1;
+        for (int i=1; i<=node; i++){
+            adjacencyList[i] = new ArrayList<>();
         }
 
-        dfs(v);
-        bfs(v);
+        for (int i=0; i<line; i++){
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
 
-        bw.write(sbDfs.toString() + "\n");
-        bw.write(sbBfs.toString());
+            adjacencyList[from].add(to);
+            adjacencyList[to].add(from);
+        }
+
+        for (int i=1; i<=node; i++){
+            Collections.sort(adjacencyList[i]);
+        }
+
+        dfsList(v);
+        sb.append("\n");
+
+        Arrays.fill(visited, false);
+
+        bfsList(v);
+
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
 
-    private static void dfs(int v) {
-        visitedDfs[v] = true;
+    private static void bfsList(int v) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.offer(v);
+        visited[v] = true;
 
-        sbDfs.append(v + " ");
+        sb.append(v +" ");
 
-        for(int i=1; i<=node; i++){
-            if(adjacencyMatrix[v][i] == 1 && !visitedDfs[i]){
-                visitedDfs[i] = true;
-                dfs(i);
+        while (!queue.isEmpty()){
+            int current = queue.poll();
+
+            for(int node : adjacencyList[current]){
+                if(!visited[node]){
+                    queue.offer(node); // 다음 레벨 방문을 위해 저장
+                    visited[node] = true;
+
+                    sb.append(node +" ");
+                }
             }
         }
     }
 
-    private static void bfs(int v) {
-        Queue<Integer> queue = new LinkedList<Integer>();
-        queue.offer(v);
+    private static void dfsList(int v) {
+        visited[v] = true;
 
-        visitedBfs[v] = true;
+        sb.append(v +" ");
 
-        sbBfs.append(v +" ");
-
-        while (!queue.isEmpty()){
-            int temp = queue.poll();
-
-            for (int i=1; i<=node; i++){
-                if(adjacencyMatrix[temp][i] == 1 && !visitedBfs[i]){
-                    queue.offer(i);
-                    visitedBfs[i] = true;
-
-                    sbBfs.append(i +" ");
-                }
+        for(int node : adjacencyList[v]){
+            if(!visited[node]){
+                dfsList(node);
             }
         }
     }
