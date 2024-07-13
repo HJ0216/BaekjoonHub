f59cb6214517da5f1cb21f322dca2ce614a93d26
Hyunji <6120hj@gmail.com>
2024-07-13 18:59:34 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 480 ms, Memory: 117516 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,13 +1,22 @@
 import java.io.*;
 import java.util.*;
 
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     static int[][] graph;
+//    static ArrayList<Integer>[] graph;
     static boolean[] isVisited;
     static int M, N;
     static int count;
 
     public static void main(String[] args) throws IOException {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        // Press Shift+F9 to start debugging your code. We have set one breakpoint
+        // for you, but you can always add more by pressing Ctrl+F8.
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -29,21 +38,31 @@ public class Main {
 
         for(int i=1; i<=N; i++){
             if(!isVisited[i]){
-                dfs(i);
+                bfs(i);
                 count++;
             }
         }
 
         bw.append(count + "\n");
+
         bw.close();
     }
 
-    private static void dfs(int v) {
-        isVisited[v] = true;
+    private static void bfs(int start){
+        Queue<Integer> queue = new LinkedList<Integer>();
+        queue.add(start);
 
-        for(int i=1; i<=N; i++){
-            if(graph[v][i] == 1 && !isVisited[i])
-                dfs(i);
+        isVisited[start] = true;
+
+        while (!queue.isEmpty()){
+            int temp = queue.poll();
+
+            for (int i=1; i<=N; i++){
+                if(graph[temp][i] == 1 && !isVisited[i]){
+                    queue.offer(i);
+                    isVisited[i] = true;
+                }
+            }
         }
     }
 }
