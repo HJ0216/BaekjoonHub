788234150045c52c754ab17c98d0fd7877a18ab7
Hyunji <6120hj@gmail.com>
2024-08-31 08:24:11 +0900

    [level 1] Title: 하샤드 수, Time: 0.16 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(int x) {
+        String[] nToStringArray = String.valueOf(x).split("");
+        int sum = 0;
+        for (String s : nToStringArray){
+            sum += Integer.parseInt(s);
+        }
+        
+        return (x%sum == 0) ? true : false;
+    }
+}
